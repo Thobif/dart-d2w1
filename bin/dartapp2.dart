7f68f9983b6835d7e75bf4dde95d451b1e802d6f
Thobif <jirayootpandee537@gmail.com>
2021-11-04 16:17:38 +0700

    new update

---
+++ bin/dartapp2.dart
@@ -0,0 +1,7 @@
+void main(){
+  //Rectangle
+  double lenght = 50;
+  double width = 40;
+
+  print('Area of Rectangle is $lenght x $width = ${lenght * width}');
+}
---
+++ bin/hello.dart
@@ -0,0 +1,8 @@
+
+void main() {
+  var name = "Jhon";
+  var age = 25;
+  print("I am " + name);
+  print("I am $name and $age year old " );
+  
+}
